8b86e0700c739e4e971af0ce5dcd59aa1b6e835a
LGrok <luolin.work@gmail.com>
2024-04-25 15:12:49 +0800

    change way to get config path if config is none

--- myBrowser/src/project/browser_manager.py
+++ myBrowser/src/project/browser_manager.py
@@ -1,8 +1,12 @@
-from myBrowser.src.utils.config_loader import load_config
+from myBrowser.src.utils.config_loader import load_config,get_config_path
 from selenium import webdriver
+import os
 
 class BrowserManager:
-    def __init__(self, config_path='test_config.json'):
+    driver: webdriver.Chrome
+    def __init__(self, config_path=None):
+        if config_path == None:
+            config_path = get_config_path()
         self.config = load_config(config_path)
         self.driver_path = self.config.get('driver_path')
         self.options = self.config.get('options', [])
