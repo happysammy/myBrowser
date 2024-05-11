2463c85bfcfe10a77d6af55e2ec654eb482144e0
LGrok <luolin.work@gmail.com>
2024-05-11 15:01:52 +0800

    增加一般浏览器启动，并适配浏览器配置文件启动范围

--- myBrowser/main.py
+++ myBrowser/main.py
@@ -2,11 +2,14 @@ import csv
 import argparse
 from concurrent.futures import ThreadPoolExecutor
 from myBrowser.src.project.twitter_automation import TwitterAutomation
+from myBrowser.src.project.common_auto import CommonAutomation
 from src.utils.config_loader import get_config_path
 
 def create_task(task_type, **kwargs):
     if task_type == "twitter":
         return TwitterAutomation(**kwargs)
+    if task_type == "common":
+        return CommonAutomation(**kwargs)
 
     else:
         raise ValueError(f"Unsupported task type: {task_type}")
@@ -21,16 +24,30 @@ def load_tasks_from_csv(file_path):
             tasks.append(create_task(task_type, **row))
     return tasks
 
+
 def parse_task_args(task_args):
     tasks = []
     for arg in task_args:
         task_info = arg.split(':')
         task_type = task_info[0]
         task_kwargs = {}
-        for kv in task_info[1:]:
-            key, value = kv.split('=')
-            task_kwargs[key] = value
-        tasks.append(create_task(task_type, **task_kwargs))
+
+        if "-" in arg:
+            index_range = task_info[1].split('-')
+            start_index = int(index_range[0])
+            end_index = int(index_range[1]) + 1  # 包括结束索引
+            for index in range(start_index, end_index):
+                task_kwargs['user_data_index'] = index
+                for kv in task_info[2:]:
+                    key, value = kv.split('=')
+                    task_kwargs[key] = value
+                tasks.append(create_task(task_type, **task_kwargs))
+        else:
+            for kv in task_info[1:]:
+                key, value = kv.split('=')
+                task_kwargs[key] = value
+            tasks.append(create_task(task_type, **task_kwargs))
+
     return tasks
 
 def main(tasks):
@@ -44,7 +61,9 @@ def main(tasks):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Social Media Automation Script')
     parser.add_argument('-f', '--file', help='Path to the CSV file containing task data', required=False)
-    parser.add_argument('-t', '--tasks', nargs='+', action='append', help='Tasks with their parameters in task_type:key=value format', required=False)
+    parser.add_argument('-t', '--tasks', nargs='+', action='append',
+                        help='Tasks with their parameters in task_type:key=value format or task_type:start-stop:key=value',
+                        required=False)
 
     args = parser.parse_args()
 
